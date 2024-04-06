ec99c08be6f5b362465433b286854fe14f75c95a
prashant jha <prasshantj2002@gmail.com>
2024-04-06 23:22:47 +0530

    this my traffic light project

---
+++ app.js
@@ -0,0 +1,11 @@
+let color="red";
+// trafic light system
+if (color=="red");{
+console.log("stop!");
+}
+if (color=="yellow");{
+    console.log("Go slow");
+}
+if (color=="green");{
+    console.log("Go");
+}
